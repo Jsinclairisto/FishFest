134888c88f0c0ce89729aa71daeea9f3d4a94882
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-04-01 18:03:01 -0700

    The player fish can now swim.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float anglePerSecond;
+    [SerializeField] private float swimForce;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector3 rotation = transform.localEulerAngles;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rotation.z += Time.deltaTime * anglePerSecond;
+            transform.localEulerAngles = rotation;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rotation.z -= Time.deltaTime * anglePerSecond;
+            transform.localEulerAngles = rotation;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * swimForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+}
