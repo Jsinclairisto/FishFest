3cbe6413450c1787395561273852300483cb715a
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-04-07 19:29:27 -0700

    Menu screen done.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,6 +7,7 @@ public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
     
+    
     // Update is called once per frame
     void Update()
     {
@@ -18,13 +19,28 @@ public class LevelLoader : MonoBehaviour
 
     public void LoadNextLevel() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+        else 
+        {
+            StartCoroutine(LoadCurrentLevel());
+        }
     }
 
     IEnumerator LoadLevel(int levelIndex) 
     {
         transition.SetTrigger("Start");
-        yield return new WaitForSeconds(.40f);
+        yield return new WaitForSeconds(.50f);
+        SceneManager.LoadScene(levelIndex);
     }
+    IEnumerator LoadCurrentLevel() 
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(.50f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 
 }
